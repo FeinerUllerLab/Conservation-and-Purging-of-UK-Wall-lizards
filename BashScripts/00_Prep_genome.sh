a5d4d94b32d3c837ee2adfbdd6a289abce57b0ea
NathalieFeiner <63855353+NathalieFeiner@users.noreply.github.com>
2026-04-06 20:04:56 +0200

    Add files via upload

---
+++ BashScripts/00_Prep_genome.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Objective: Prepare the reference genome for the downstream analyses
+
+module load samtools
+samtools faidx genome.fna
+
+module load bwa
+bwa index genome.fna
+
+PICARD_HOME=/pdc/software/eb/software/picard/2.25.5
+java -Xmx16g -jar $PICARD_HOME/picard.jar CreateSequenceDictionary R=genome.fna O=genome.dict
